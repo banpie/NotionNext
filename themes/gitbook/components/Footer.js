275137f15a078830c663f066c8cce45d2ebca06c
Banpie <banpie@bpteach.com>
2024-01-27 23:25:45 +0800

    移除footer注脚（使用正确的注释代码）

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -23,7 +23,7 @@ const Footer = ({ siteInfo }) => {
                 <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
             <span className='pl-2 hidden busuanzi_container_site_uv'>
                 <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-            // <h1 className='pt-1'>{siteConfig('TITLE')}</h1>
+            /** <h1 className='pt-1'>{siteConfig('TITLE')}</h1>**/
 
         </footer>
   )
